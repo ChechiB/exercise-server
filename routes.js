65eeab0200c7632475d5f9d14782cb7546138b06
chechiB <chechi.fx2@gmail.com>
2021-03-29 04:46:01 -0300

    rv:dist

--- routes.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-exports.__esModule = true;
-exports.init = void 0;
-var item_router_js_1 = require("./src/items/item.router.js");
-var init = function (app) {
-    app.use("/api/items", item_router_js_1.itemsRouter);
-};
-exports.init = init;
