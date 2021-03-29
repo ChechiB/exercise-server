342ee3bc29914851ba5b745bbd17882e33a2776c
chechiB <chechi.fx2@gmail.com>
2021-03-29 04:44:37 -0300

    add: handler error

--- dist/items/item.router.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-module.exports = router;
-const ctrl = require('./item.controller');
-const { searchSchema, paramSchema } = require('./item.validation');
-router.get('/', ctrl.validateParams({ schema: searchSchema }), ctrl.search);
-router.get('/:id', ctrl.validateParams({ schema: paramSchema }), ctrl.getItem);
--- dist/routes.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.init = void 0;
-const item_router_js_1 = require("./src/items/item.router.js");
-const init = (app) => {
-    app.use(`/api/items`, item_router_js_1.itemsRouter);
-};
-exports.init = init;
-//# sourceMappingURL=routes.js.map
